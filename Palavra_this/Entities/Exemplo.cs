77e4f8d3bc01b2974201a116df8e424a33dd20a1
Keverson CArvalho <kalmeida743@gmail.com>
2021-12-01 13:29:16 -0300

    Adicionado exercicio com a palavra this

---
+++ Palavra_this/Entities/Exemplo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Palavra_this.Entities
+{
+    class Exemplo
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public Exemplo()
+        {
+            Quantidade = 0;
+        }
+
+        public Exemplo(string nome, double preco): this()
+        {
+            Preco = preco;
+            Nome = nome;
+        }
+
+        public Exemplo(string nome, double preco, int quantidade):this(nome,preco)
+        {
+            Quantidade = quantidade;
+        }
+    }
+}
