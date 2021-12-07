ddcf5365b19362526457920b34d54c0e6189f29a
Keverson CArvalho <kalmeida743@gmail.com>
2021-12-07 11:59:31 -0300

    Adicionado uma discussão sobre o params(parâmetros variáveis para uma função) e o ref(passagem de um tipo struct como referência

---
+++ ParamsChanger/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParamsChanger
+{
+    class Calculator
+    {
+        public static int Sum(params int[] numbers)
+        {
+            int sum = 0;
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+
+        public static void Triple(ref int x)
+        {
+            x *= 3;
+        }
+    }
+}
---
+++ ParamsChanger/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ParamsChanger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Definição de função com parâmetros variados
+            int s1 = Calculator.Sum(2, 3);
+            int s2 = Calculator.Sum(2, 4, 6);
+            int s3 = Calculator.Sum(2, 4, 3, 7);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+
+            int x = 10;
+            Calculator.Triple(ref x);
+            Console.WriteLine(x);
+        }
+    }
+}
